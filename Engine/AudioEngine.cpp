dd5678d38a83bc5928ef34b95177b383a20affa1
Conner Pittaway <57526209+ConnerPittaway@users.noreply.github.com>
2023-01-09 04:51:09 +0000

    Bugs with Audio Engine Fixed (Refactoring/Additional Features Needed)
    Bug associated with FMod3D fixed, audio now has positional data but more functionality and abstraction is needed to pass in sound/sprite info

--- Engine/AudioEngine.cpp
+++ Engine/AudioEngine.cpp
@@ -27,6 +27,7 @@ bool AudioEngine::init()
 	//One listener (player)
 	fmodSystem->set3DNumListeners(1);
 
+	
 	//Default factors
 	fmodSystem->set3DSettings(1, 1, 1);
 
@@ -39,16 +40,20 @@ void AudioEngine::update()
 { 
 	//updateListenerPositon(0, 0);
 
-	//Position of listenere
+	//Position of listener
 	listenerPosition = FMOD_VECTOR(0, 0, 0);
 	
 	//forward/up vectors left at default
-	const auto forward = FMOD_VECTOR(1, 1, 1);
-	const auto up = FMOD_VECTOR(2, 2, 2);
+	FMOD_VECTOR forward = FMOD_VECTOR(0, 0, 0);
+	const FMOD_VECTOR up = FMOD_VECTOR(0, 0, 0);
 
 	//Set listener (player) position
-	if (const auto errorCode = fmodSystem->set3DListenerAttributes(0, &listenerPosition, 0, &forward, &up); errorCode != FMOD_OK)
-		LOG_ERROR("Failed to set 3D listener attributes, ERROR: " + std::to_string(errorCode));
+	FMOD_RESULT fmodResult;
+	fmodResult = fmodSystem->set3DListenerAttributes(0, 0, 0, 0, 0);
+	if (fmodResult != FMOD_OK)
+	{
+		LOG_ERROR("FMod failed to set listener 3D");
+	}
 
 	//System must be updated once per cycle
 	fmodSystem->update(); 
@@ -84,18 +89,34 @@ bool AudioEngine::playSound(const char *path, bool isPaused, float volume, float
 	fmodChannel->setMode(FMOD_3D);
 	
 	//Audio source position
-	FMOD_VECTOR sourcePosition = {100000, 100000, 1000000};
+	FMOD_VECTOR sourcePosition = {0, 1, 1};
 	std::cout << "Source: " << sourcePosition.x << " " << sourcePosition.y << " " << sourcePosition.y << "\n";
 	std::cout << "Listener: " << listenerPosition.x << " " << listenerPosition.y << " " << listenerPosition.y << "\n";
 
+	fmodChannel->setMode(FMOD_3D);
+	FMOD_RESULT fmodResult;
+	fmodResult = fmodChannel->setVolume(volume);
+	if (fmodResult != FMOD_OK)
+	{
+		LOG_ERROR("FMod failed to set volume");
+		return false;
+	}
 	//Set audio position
-	if (const auto errorCode = fmodChannel->set3DAttributes(&sourcePosition, nullptr); errorCode != FMOD_OK)
-		LOG_ERROR("Failed to set 3D attributes, ERROR: " + std::to_string(errorCode));
 	
+	fmodResult = fmodChannel->set3DAttributes(&sourcePosition, nullptr);
+	if (fmodResult != FMOD_OK)
+	{
+		LOG_ERROR("FMod failed to set attributes 3D");
+		return false;
+	}
+
 	//Set min/max falloff
-	if (const auto errorCode = fmodChannel->set3DMinMaxDistance(1, 2000000); errorCode != FMOD_OK)
-		LOG_ERROR("Failed to set min/max distance, ERROR: " + std::to_string(errorCode));
-	
+	fmodResult = fmodChannel->set3DMinMaxDistance(1, 10);
+	if (fmodResult != FMOD_OK)
+	{
+		LOG_ERROR("FMod failed to set distance");
+		return false;
+	}
 	return true;
 }
 
@@ -103,11 +124,11 @@ void AudioEngine::onDebugEvent(const OnDebugEventChanged* event)
 {
 	if (event->key == DebugPlaySound)
 	{
-		Instance()->loadSound("Sounds\\griddy.mp3", FMOD_DEFAULT);
-		Instance()->playSound("Sounds\\griddy.mp3", false, 0.01f, 10000, 10000);
+		Instance()->loadSound("Sounds\\griddy.mp3", FMOD_3D);
+		Instance()->playSound("Sounds\\griddy.mp3", false, 0.1f, 100, 100);
 
-		Instance()->loadSound("Sounds\\doneit.mp3", FMOD_DEFAULT);
-		Instance()->playSound("Sounds\\doneit.mp3", false, 0.01f, 100000, 10000);
+		Instance()->loadSound("Sounds\\doneit.mp3", FMOD_3D);
+		//Instance()->playSound("Sounds\\doneit.mp3", false, 0.1f, 1000000000000, 100000000000);
 	}
 }
 
