8675c27cd6ee94b6c31b92c0c031de4e7569f6f7
Lewis Powell <57398782+Lewis-Powell@users.noreply.github.com>
2023-01-16 14:31:16 +0000

    Removing commented debug code and extra sort

--- Game/ScoreSystem.cpp
+++ Game/ScoreSystem.cpp
@@ -11,22 +11,15 @@ ScoreSystem::ScoreSystem()
 void ScoreSystem::SaveScore(std::string Username)
 {
 
-	//std::fstream newfile;
-	//std::fstream file;
-
 	file.open("Score.txt", std::ios::app);
 	if (file)
 	{
-		//std::cout << "File Exists";
-		//file.open
-		//file.open("Score.txt", std::ios::app);
 		file << Username << "-" << currentScore << "\n";
 		file.close();
 
 	}
 	else
 	{
-		//file.close();
 		std::cout << "File Doesn't Exist";
 		file.open("Score.txt", std::ios::out);
 		file << Username << "-" << currentScore << "\n";
@@ -38,36 +31,20 @@ void ScoreSystem::SaveScore(std::string Username)
 
 void ScoreSystem::ReadScores()
 {
-	//Read the Scores sort from high to low and put the top 10 on the screen if there is 10
-	//Once sorted is it worth writing them back to the file in a sorted order?
- 
-	
-	SortScores();
-	
-	/*file.open("Score.txt", std::ios::in);
-
-	file.close();*/
-
 	file.open("Score.txt");
-	int lineCount = 0;
 	std::string line, m_Name;
 	size_t pos = 0;
 	std::string delim = "-";
 
 	for (int i = 0; i < 11 && std::getline(file, line); i++)
 	{
-		//std::cout << line << std::endl;
 		while ((pos = line.find(delim)) != std::string::npos)
 		{
 			m_Name = line.substr(0, pos);
-			//std::cout << m_Name << " :token1" << std::endl;
 			m_FileData[i].Name = m_Name;
 			line.erase(0, pos + delim.length());
 			m_FileData[i].Score = std::stoi(line);
 		}
-		//std::cout << line << "outside while" << std::endl;
-		//std::cout << m_FileData[i].Name << "\n" << m_FileData[i].Score << std::endl;
-
 	}
 	file.close();
 	SortScores();
