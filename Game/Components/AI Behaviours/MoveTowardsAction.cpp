25ff7baec7c2c254d633361099104a6ddb6648dc
Josh <deadmonstor69@gmail.com>
2023-02-02 10:21:11 +0000

    Auto stash before merge of "cleanup-engine" and "origin/AI-Behaviours"

--- Game/Components/AI Behaviours/MoveTowardsAction.cpp
+++ Game/Components/AI Behaviours/MoveTowardsAction.cpp
@@ -1,5 +1,7 @@
 #include "MoveTowardsAction.h"
-#include "Core\Components\Transform.h"
+
+#include "../TurnManager.h"
+#include "Core/Components/Transform.h"
 
 MoveTowardsAction::MoveTowardsAction()
 	: target(glm::vec2())
@@ -19,16 +21,15 @@ void MoveTowardsAction::Act()
 	if (parentObject != nullptr)
 	{
 		currentPos = parentObject->getTransform()->getPosition();
-		std::deque<TileHolder*> path = PathfindingMachine::Instance()->FindPath(currentPos, target);
+		const std::deque<TileHolder*> path = PathfindingMachine::Instance()->FindPath(currentPos, target);
 		if (!path.empty())
 		{
-			glm::vec2 tileSize = GridSystem::Instance()->getTileSize();
-			GridSystem::Instance()->resetSatOnTile(0, parentObject->getTransform()->getPosition()/tileSize);
-			parentObject->getTransform()->setPosition(path.front()->position * tileSize);
-			//Set grid system "satOnTile" values
-
-			GridSystem::Instance()->setSatOnTile(0, parentObject->getTransform()->getPosition()/tileSize,parentObject);
+			GridSystem* instance = GridSystem::Instance();
+			instance->resetSatOnTile(0,  instance->getTilePosition(parentObject->getTransform()->getPosition()));
+			parentObject->getTransform()->setPosition(instance->getWorldPosition(path.front()->position));
+			instance->setSatOnTile(0, instance->getTilePosition(parentObject->getTransform()->getPosition()), parentObject);
 		}
 	}
-	
+
+	TurnManager::Instance()->endTurn();
 }
