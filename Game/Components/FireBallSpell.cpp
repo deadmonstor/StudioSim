7b3a641ea672ab76bd5e127af83df5eebcb0141a
Fayzaan Ali <57409767+fayzaanali@users.noreply.github.com>
2023-02-02 13:34:02 +0000

    updated fireballspell class

--- Game/Components/FireBallSpell.cpp
+++ Game/Components/FireBallSpell.cpp
@@ -1,8 +1,19 @@
 #include "FireBallSpell.h"
-#include "Core/Grid/GridSystem.h"
-#include <Core/Components/Health.h>
-#include "DestroyAfterAnimation.h"
-#include "Core/Components/Transform.h"
+#include "FireballComponent.h"
+
+FireBallSpell::FireBallSpell()
+{
+	fireBallStats = new SpellsStats();
+	fireBallStats->name = "Fire Ball";
+	fireBallStats->cost = 10;
+	fireBallStats->manaCost = 2;
+	fireBallStats->damagePerTurn = 1;
+	fireBallStats->cooldown = 1;
+	fireBallStats->spellPower = 1;
+	fireBallStats->spellCooldown = 1;
+	fireBallStats->range = 5;
+	
+}
 
 void FireBallSpell::UseSpellAt(glm::fvec2 playerPos, glm::fvec2 attackDir)
 {
@@ -14,11 +25,12 @@ void FireBallSpell::CreateSpell(glm::fvec2 pos)
 	const TileHolder* curTileHolder = GridSystem::Instance()->getTileHolder(0, pos);
 	GameObject* gameObject = curTileHolder->gameObjectSatOnTile;
 
-
 	const glm::fvec2 worldPos = GridSystem::Instance()->getWorldPosition(pos);
-	GameObject* spell = SceneManager::Instance()->createGameObject("Spell", worldPos);
+	GameObject* spell = SceneManager::Instance()->createGameObject("Fireball", worldPos);
 	spell->getTransform()->setSize(glm::vec2(48, 48));
-	AnimatedSpriteRenderer* spellSprite = spell->addComponent<AnimatedSpriteRenderer>(FireBallTexture, 0.05f);
-	spellSprite->setPivot(Pivot::Center);
+	/*AnimatedSpriteRenderer* spellSprite = spell->addComponent<AnimatedSpriteRenderer>(FireBallTexture, 0.05f);*/
+	/*spellSprite->setPivot(Pivot::Center);*/
 	spell->addComponent<DestroyAfterAnimation>();
+	spell->addComponent<FireballComponent>();
+
 }
--- Game/Components/FireBallSpell.h
+++ Game/Components/FireBallSpell.h
@@ -2,11 +2,17 @@
 #include "Stats.h"
 #include "glm/fwd.hpp"
 #include "Core/Renderer/ResourceManager.h"
+#include "Core/Grid/GridSystem.h"
+#include <Core/Components/Health.h>
+#include "DestroyAfterAnimation.h"
+#include "Core/Components/Transform.h"
 
 class FireBallSpell : public SpellsStats
 {
+	FireBallSpell();
 	void UseSpellAt(glm::fvec2 playerPos, glm::fvec2 attackDir);
 	void CreateSpell(glm::fvec2 pos);
-	const std::vector<Texture> FireBallTexture = ResourceManager::GetTexturesContaining("Fire Sprites");
+	const std::vector<Texture> FireBallTexture = ResourceManager::GetTexturesContaining("Fireball");
+	SpellsStats* fireBallStats;
 };
 
