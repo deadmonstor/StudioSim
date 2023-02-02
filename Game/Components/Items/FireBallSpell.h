77f2bb659fce6df8411f56d834c91af186544c93
Fayzaan Ali <57409767+fayzaanali@users.noreply.github.com>
2023-02-02 15:23:33 +0000

    Update FireBallSpell.h

--- Game/Components/Items/FireBallSpell.h
+++ Game/Components/Items/FireBallSpell.h
@@ -1,7 +1,5 @@
 #pragma once
 #include "Stats.h"
-#include "glm/fwd.hpp"
-#include "Core/Renderer/ResourceManager.h"
 #include "Core/Grid/GridSystem.h"
 #include <Core/Components/Health.h>
 #include "Core/Components/Transform.h"
