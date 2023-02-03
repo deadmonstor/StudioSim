49ce58949be66aed0f851bc26d53c56af132d77c
Charlotte Marriner <57407155+charlottemarriner@users.noreply.github.com>
2023-02-03 15:13:13 +0000

    Add enemy loot tings

--- Game/LootTable.cpp
+++ Game/LootTable.cpp
@@ -50,3 +50,53 @@ std::string LootTable::RollLoot()
 	}
 	
 }
+
+void EnemyDropLootTable::EnemyDropLoadingIntoLootTableArray()
+{
+	file.open("EnemyDropLootTable.txt");
+	std::string line, m_Name;
+	size_t pos = 0;
+	std::string delim = "-";
+
+	for (int i = 0; i < 11 && std::getline(file, line); i++)
+	{
+		while ((pos = line.find(delim)) != std::string::npos)
+		{
+			m_Name = line.substr(0, pos);
+			m_EnemyDropLootTableData[i].ItemName = m_Name;
+			line.erase(0, pos + delim.length());
+			m_EnemyDropLootTableData[i].DropChance = std::stoi(line);
+		}
+	}
+	if (m_EnemyDropLootTableData[0].ItemName == "money")
+	{
+		std::cout << "Working\n";
+	}
+	EnemyDropRollLoot();
+}
+
+std::string EnemyDropLootTable::EnemyDropRollLoot()
+{
+
+	//Roll random number 0-100. rand % 100
+	srand(time(0));
+
+	//random number
+	int random = (rand() % 100) + 1;
+	int temp = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		temp += m_EnemyDropLootTableData[i].DropChance;
+		if (random < temp)
+		{
+			//Output the item
+			std::cout << m_EnemyDropLootTableData[i].ItemName;
+			std::cout << random;
+			std::string rolledLoot = m_EnemyDropLootTableData[i].ItemName;// this string return ting
+			std::cout << "\n";
+			return rolledLoot;
+			break;
+		}
+	}
+
+}
